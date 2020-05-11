3af4c9e4de9812b4db02cd2b9867f1544783eb8e
Columbine21 <ziqiyuan@bupt.edu.cn>
2020-05-12 00:16:20 +0800

    basic function finished

--- assignment5.4/assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
+++ assignment5.4/assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
@@ -1,19 +1,73 @@
 package com.bytedance.videoplayer;
 
+import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MyVideoView videoView;
+    private MediaController mController;
+    private static final String TAG = "yuanziqi";
+    private static boolean isHorizonal = false;
+    private static int progressRecorder;
+    private static boolean isPlaying;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = findViewById(R.id.imageView);
-        String url = "https://s3.pstatp.com/toutiao/static/img/logo.271e845.png";
-        Glide.with(this).load(url).into(imageView);
+
+        videoView = findViewById(R.id.videoview);
+        videoView.setConfig(isHorizonal);
+        mController = new MediaController(this);
+
+        videoView.setVideoPath(getVideoPath(R.raw.bytedance));
+        videoView.setMediaController(mController);
+        mController.setMediaPlayer(videoView);
+
+
+        if (savedInstanceState == null)
+        {
+            progressRecorder = 0;
+            isPlaying = false;
+        }
+        else
+        {
+            Log.d(TAG, "Recover from: " + progressRecorder);
+            videoView.seekTo(progressRecorder);
+            if (isPlaying)
+                videoView.start();
+        }
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        isHorizonal = !isHorizonal;
+        videoView.setConfig(isHorizonal);
+        Log.d(TAG, "Configuration changes. isHorizonal: " + String.valueOf(isHorizonal));
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        progressRecorder = this.videoView.getCurrentPosition();
+        isPlaying = this.videoView.isPlaying();
+        Log.d(TAG, String.valueOf(progressRecorder) + String.valueOf(isPlaying));
+    }
+
+    private String getVideoPath(int resId)
+    {
+        return "android.resource://" + this.getPackageName() + "/" + resId;
     }
 }
---
+++ assignment5.4/assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MyVideoView.java
@@ -0,0 +1,45 @@
+package com.bytedance.videoplayer;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.widget.VideoView;
+
+public class MyVideoView extends VideoView {
+
+    private boolean config = false;
+
+    public void setConfig(boolean config) {
+        this.config = config;
+    }
+
+    public MyVideoView(Context context) {
+        super(context);
+    }
+
+    public MyVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public MyVideoView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (config) {
+            int width=getDefaultSize(0,widthMeasureSpec);
+            int height=getDefaultSize(0,heightMeasureSpec);
+            setMeasuredDimension(width,height);
+        }
+        else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+}
